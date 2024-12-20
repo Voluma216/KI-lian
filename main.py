6e1a256902faa8948f1da480f4d361d9faa87d87
Voluma216 <frankest3@gmail.com>
2024-12-20 18:41:14 +0100

    Inital commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+def main():
+    base_url = "https://www.lottozahlenonline.de/statistik/beide-spieltage/lottozahlen-archiv.php?j={}"
+
+def foo(x: int):
+    if x > 42 and x < 100:
+        return True
+    else:
+        return False
+    
+def bar(x):
+    match x:
+        case 1:
+            return True
+        case 2:
+            return False
+        case _:
+            return True
+
+if __name__ == "__main__":
+    main()
+    
